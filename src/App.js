11b95e8543bfb52c7fa465add4b8b9c1e7ac8ee1
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-12 14:12:50 -0800

    commit new stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: 'Ben', lastName: 'Linn' },
+      company: 'Taco World',
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Hi {this.state.name.firstName}
             {this.state.name.lastName}, I work at {this.state.company}
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Monsters-Rolodex
-        </a>
-      </header>
-    </div>
-  );
+          </p>
+          <button
+            onClick={() => {
+              this.setState({ name: 'Easton' });
+              console.log(this.state);
+            }}>
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
