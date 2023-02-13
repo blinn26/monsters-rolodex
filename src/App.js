646108a20929bae44c9ca389c9c9ab7fd34a98cd
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-13 14:45:20 -0800

    afternoon before going home from work

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
     console.log('constructor');
   }
@@ -28,6 +29,11 @@ class App extends Component {
 
   render() {
     console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className='App'>
         <input
@@ -35,25 +41,13 @@ class App extends Component {
           type='search'
           placeholder='search monsters'
           onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            // [ {name: 'Bob',}, {name: 'Alice',} ]
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
             });
-
-            this.setState(
-              () => {
-                return { monsters: filteredMonsters };
-              },
-              () => {
-                console.log({ endingArray: this.state.monsters });
-              }
-            );
           }}
         />
-
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
