d3164d93f2fdfa15fb8d015ec392b69c1481c67b
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-13 13:45:44 -0800

    afternoon tweaks

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -25,8 +27,27 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            // [ {name: 'Bob',}, {name: 'Alice',} ]
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
+
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
